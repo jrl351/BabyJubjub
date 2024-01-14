fea18a815f071f69a1aefc9552cff4cf4edea249
Justin LeBlanc <jleblanc@polygon.technology>
2024-01-14 18:55:47 +0000

    Updates to Makefile

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ rm -rf libs/libbabyjubjub*.a
 rm -rf BabyJubjub.xcframework
 rm -f libbabyjubjub.zip
 
+mkdir -p libs
+
 echo "Building libraries..."
 cargo build --release --lib --target aarch64-apple-darwin
 cargo build --release --lib --target x86_64-apple-darwin
@@ -13,9 +15,9 @@ lipo -create \
   target/aarch64-apple-darwin/release/libbabyjubjub.a \
   -output libs/libbabyjubjub-macos.a
 
-cargo build --release --lib --target aarch64-apple-ios-sim &&
-cargo build --release --lib --target aarch64-apple-ios && 
-cargo build --release --lib --target x86_64-apple-ios &&
+cargo build --release --lib --target aarch64-apple-ios-sim
+cargo build --release --lib --target aarch64-apple-ios 
+cargo build --release --lib --target x86_64-apple-ios
 
 cp target/aarch64-apple-ios/release/libbabyjubjub.a libs/libbabyjubjub-ios.a
 lipo -create \
