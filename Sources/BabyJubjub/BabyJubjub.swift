65d8166635dfcd1be3cee8b1800a637e6bbb6f22
Justin LeBlanc <jleblanc@polygon.technology>
2024-06-03 16:35:28 +0100

    Make functions to call the rest of the BJJ functionality, with tests to make sure the functions actually run

--- Sources/BabyJubjub/BabyJubjub.swift
+++ Sources/BabyJubjub/BabyJubjub.swift
@@ -7,105 +7,261 @@ import os
 
 /// Swift wrapper for the Rust Baby Jubjub library
 public enum BabyJubjub {
-    /// Generate a public key from a private key
-    /// - Parameters:
-    ///     - hexString: the private key as a hex string
-    public static func privateKeyToPublicKey(hexString privateKey: String) -> String {
-        let privateKeyCString = privateKey.withCString { $0 }
-        guard let publicKeyCString = prv2pub(privateKeyCString) else {
-            Logger().log("Returned invalid string from prv2pub")
-            return ""
+    /**
+     Compress a signature
+     - Parameter signature: The signature
+     - Returns: If successful, the compressed signature.  If not, nil
+     */
+    public static func packSignature(signature: String) -> String? {
+        let signatureCString = signature.withCString { $0 }
+        guard let packedSignatureCString = pack_signature(signatureCString),
+              let packedSignature = String(validatingUTF8: packedSignatureCString) else {
+            Logger().log("Returned invalid string from pack_signature")
+            return nil
         }
         
-        let publicKey = String(validatingUTF8: publicKeyCString) ?? ""
+        packedSignatureCString.deallocate()
+        
+        return packedSignature
+    }
+    
+    /**
+     Decompress a signature
+     - Parameter compressedSignature: The compressed signature
+     - Returns: If successful, the decompressed signature.  If not, nil
+     */
+    public static func unpackSignature(compressedSignature: String) -> String? {
+        let compressedSignatureCString = compressedSignature.withCString { $0 }
+        guard let signatureCString = unpack_signature(compressedSignatureCString),
+              let signature = String(validatingUTF8: signatureCString) else {
+            Logger().log("Returned invalid string from unpack_signature")
+            return nil
+        }
+        
+        signatureCString.deallocate()
+        
+        return signature
+    }
+    
+    /**
+     Compress a point
+     - Parameter pointX: The X coordinate of the point
+     - Parameter pointY: The Y coordinate of the point
+     - Returns: If successful, the compressed point. If not, nil
+     */
+    public static func packPoint(pointX: String, pointY: String) -> String? {
+        let pointXCString = pointX.withCString { $0 }
+        let pointYCString = pointY.withCString { $0 }
+        
+        guard let packedPointCString = pack_point(pointXCString, pointYCString),
+              let packedPoint = String(validatingUTF8: packedPointCString) else {
+            Logger().log("Returned invalid string from pack_point")
+            return nil
+        }
+        
+        packedPointCString.deallocate()
+        
+        return packedPoint
+    }
+    
+    /**
+     Decompress a point
+     - Parameter compressedPoint: The compressedPoint
+     - Returns: If successful, the decompressed point.  If not, nil
+     */
+    public static func unpackPoint(compressedPoint: String) -> String? {
+        let compressedPointCString = compressedPoint.withCString { $0 }
+        guard let pointCString = unpack_point(compressedPointCString),
+              let point = String(validatingUTF8: pointCString) else {
+            Logger().log("Returned invalid string from unpack_point")
+            return nil
+        }
+        
+        pointCString.deallocate()
+        
+        return point
+    }
+    
+    /**
+     Generate a public key from a private key
+     - Parameter hexString: The private key as a hex string, 64-characters long, padded with zeros
+     - Returns: If successful, the public key.  If not, nil
+     */
+    public static func privateKeyToPublicKey(hexString privateKey: String) -> String? {
+        let privateKeyCString = privateKey.withCString { $0 }
+        guard let publicKeyCString = prv2pub(privateKeyCString),
+              let publicKey = String(validatingUTF8: publicKeyCString) else {
+            Logger().log("Returned invalid string from prv2pub")
+            return nil
+        }
         
         publicKeyCString.deallocate()
         
         return publicKey
     }
     
-    public static func hashPoseidon(input: String) -> String {
+    /**
+     Run poseidon hash with one input
+     - Parameter input: The hash input
+     - Returns: If successful, the hash.  If not, nil
+     */
+    public static func poseidonHash(input: String) -> String? {
         let inputCString = input.cString(using: .utf8)
         
-        guard let hashCString = poseidon_hash(inputCString) else {
+        guard let hashCString = poseidon_hash(inputCString),
+              let hash = String(validatingUTF8: hashCString) else {
             Logger().log("Returned invalid string from poseidon_hash()")
-            return ""
+            return nil
         }
         
-        let hash = String(validatingUTF8: hashCString) ?? ""
         hashCString.deallocate()
         
         return hash
     }
     
-    public static func hashPoseidon(input1: String, input2: String) -> String {
+    /**
+     Run poseidon hash with two inputs
+     - Parameter input1: The first hash input
+     - Parameter input2: The second hash input
+     - Returns: If successful, the hash.  If not, nil
+     */
+    public static func poseidonHash(input1: String, input2: String) -> String? {
         let input1CString = input1.cString(using: .utf8)
         let input2CString = input2.cString(using: .utf8)
         
         guard let hashCString = poseidon_hash2(input1CString,
-                                               input2CString) else {
+                                               input2CString),
+              let hash = String(validatingUTF8: hashCString) else {
             Logger().log("Returned invalid string from poseidon_hash2()")
-            return ""
+            return nil
         }
         
-        let hash = String(validatingUTF8: hashCString) ?? ""
         hashCString.deallocate()
         
         return hash
     }
     
-    public static func hashPoseidon(input1: String, input2: String, input3: String) -> String {
+    /**
+     Run poseidon hash with three inputs
+     - Parameter input1: The first hash input
+     - Parameter input2: The second hash input
+     - Parameter input3: The third hash input
+     - Returns: If successful, the hash.  If not, nil
+     */
+    public static func poseidonHash(input1: String, input2: String, input3: String) -> String? {
         let input1CString = input1.cString(using: .utf8)
         let input2CString = input2.cString(using: .utf8)
         let input3CString = input3.cString(using: .utf8)
         
         guard let hashCString = poseidon_hash3(input1CString,
                                                input2CString,
-                                               input3CString) else {
+                                               input3CString),
+              let hash = String(validatingUTF8: hashCString) else {
             Logger().log("Returned invalid string from poseidon_hash3()")
-            return ""
+            return nil
         }
         
-        let hash = String(validatingUTF8: hashCString) ?? ""
         hashCString.deallocate()
         
         return hash
     }
     
-    public static func hashPoseidon(input1: String, input2: String, input3: String, input4: String) -> String {
+    /**
+     Run poseidon hash with four inputs
+     - Parameter input1: The first hash input
+     - Parameter input2: The second hash input
+     - Parameter input3: The third hash input
+     - Parameter input4: The fourth hash input
+     - Returns: If successful, the hash.  If not, nil
+     */
+    public static func poseidonHash(input1: String, input2: String, input3: String, input4: String) -> String? {
         let input1CString = input1.cString(using: .utf8)
         let input2CString = input2.cString(using: .utf8)
         let input3CString = input3.cString(using: .utf8)
         let input4CString = input4.cString(using: .utf8)
         
-        
         guard let hashCString = poseidon_hash4(input1CString,
                                                input2CString,
                                                input3CString,
-                                               input4CString) else {
+                                               input4CString),
+              let hash = String(validatingUTF8: hashCString) else {
             Logger().log("Returned invalid string from poseidon_hash4()")
             return ""
         }
         
-        
-        let hash = String(validatingUTF8: hashCString) ?? ""
         hashCString.deallocate()
         
         return hash
     }
     
-    public static func signPoseidon(privateKeyHex: String, msg: String) -> String {
-        let privateKeyCString = privateKeyHex.cString(using: .utf8)
-        let msgCString = msg.cString(using: .utf8)
+    /**
+     Run poseidon hash with the hash of three trees
+     - Parameter claimsTree: The claims tree hash
+     - Parameter revocationTree: The revocation tree hash
+     - Parameter rootsTreeRoot: The hash of the roots tree
+     - Returns: If successful, the hash.  If not, nil
+     */
+    public static func hashPoseidon(claimsTree: String, revocationTree: String, rootsTreeRoot: String) -> String? {
+        let claimsTreeCString = claimsTree.cString(using: .utf8)
+        let revocationTreeCString = revocationTree.cString(using: .utf8)
+        let rootsTreeRootCString = rootsTreeRoot.cString(using: .utf8)
         
-        guard let signedMsgCString = sign_poseidon(privateKeyCString, msgCString) else {
+        guard let hashCString = hash_poseidon(claimsTreeCString,
+                                              revocationTreeCString,
+                                              rootsTreeRootCString),
+              let hash = String(validatingUTF8: hashCString) else {
+            Logger().log("Returned invalid string from hash_poseidon()")
+            return nil
+        }
+        
+        hashCString.deallocate()
+        
+        return hash
+    }
+    
+    /**
+     Use poseidon to sign a message
+     - Parameter privateKey: The private key as a hex string
+     - Parameter msg: The message
+     - Returns: If successful, the signed message.  If not, nil
+     */
+    public static func signPoseidon(privateKeyHex: String, message: String) -> String? {
+        let privateKeyCString = privateKeyHex.cString(using: .utf8)
+        let messageCString = message.cString(using: .utf8)
+        
+        guard let signedMsgCString = sign_poseidon(privateKeyCString, messageCString),
+              let signedMsg = String(validatingUTF8: signedMsgCString) else {
             Logger().log("Returned invalid string from poseidon_hash()")
             return ""
         }
         
-        let signedMsg = String(validatingUTF8: signedMsgCString) ?? ""
         signedMsgCString.deallocate()
         
         return signedMsg
+    }
+    
+    /**
+     Run verify a signed message with a private key
+     - Parameter privateKey: The private key
+     - Parameter compressedSignature: The signature
+     - Parameter message: The message
+     - Returns: If successful, the result.  If not, nil
+     */
+    public static func verifyPoseidon(privateKey: String, compressedSignature: String, message: String) -> Bool? {
+        let privateKeyCString = privateKey.cString(using: .utf8)
+        let compressedSignatureCString = compressedSignature.cString(using: .utf8)
+        let messageCString = message.cString(using: .utf8)
+        
+        guard let verifyCString = verify_poseidon(privateKeyCString,
+                                                  compressedSignatureCString,
+                                                  messageCString),
+              let verify = String(validatingUTF8: verifyCString) else {
+            Logger().log("Returned invalid string from hash_poseidon()")
+            return nil
+        }
+        
+        verifyCString.deallocate()
+        
+        return verify == "1"
     }
 }
--- Tests/BabyJubjubTests/BabyJubjubTests.swift
+++ Tests/BabyJubjubTests/BabyJubjubTests.swift
@@ -2,4 +2,127 @@ import XCTest
 @testable import BabyJubjub
 
 final class BabyJubjubTests: XCTestCase {
+    func testPackSignature_doesNotError() throws {
+        let signature = "1717fe".padLeft(length: 128, char: "0")
+        
+        let compressed = try XCTUnwrap(BabyJubjub.packSignature(signature: signature))
+        XCTAssertNotNil(compressed)
+    }
+    
+    func testUnpackSignature_doesNotError() throws {
+        let signature = "deadbeef".padLeft(length: 128, char: "0")
+        
+        let decompressed = BabyJubjub.unpackSignature(compressedSignature: signature)
+        XCTAssertNotNil(decompressed)
+    }
+    
+    func testPackPoint_doesNotError() throws {
+        let x = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+        let y = "2626589144620713026669568689430873010625803728049924121243784502389097019475"
+        
+        let compressed = try XCTUnwrap(BabyJubjub.packPoint(pointX: x, pointY: y))
+        XCTAssertNotNil(compressed)
+    }
+    
+    func testUnpackPoint_doesNotError() throws {
+        let hexString = "deadbeef".padLeft(length: 128, char: "0")
+        
+        let decompressed = BabyJubjub.unpackSignature(compressedSignature: hexString)
+        XCTAssertNotNil(decompressed)
+    }
+    
+    func testPrivateKeyToPublicKey_doesNotError() {
+        let hexString = "42fe".padLeft(length: 64, char: "0")
+        
+        let result = BabyJubjub.privateKeyToPublicKey(hexString: hexString)
+        XCTAssertNotNil(result)
+    }
+    
+    func testPoseidonHash_WithOneInput_doesNotError() {
+        let input = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+        
+        let result = BabyJubjub.poseidonHash(input: input)
+        XCTAssertNotNil(result)
+    }
+    
+    func testPoseidonHash_WithTwoInputs_doesNotError() {
+        let input1 = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+        let input2 = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+
+        let result = BabyJubjub.poseidonHash(input1: input1, input2: input2)
+        XCTAssertNotNil(result)
+    }
+    
+    func testPoseidonHash_WithThreeInputs_doesNotError() {
+        let input1 = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+        let input2 = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+        let input3 = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+
+        let result = BabyJubjub.poseidonHash(input1: input1, input2: input2, input3: input3)
+        XCTAssertNotNil(result)
+    }
+    
+    func testPoseidonHash_WithFourInputs_doesNotError() {
+        let input1 = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+        let input2 = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+        let input3 = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+        let input4 = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+
+        let result = BabyJubjub.poseidonHash(input1: input1,
+                                             input2: input2,
+                                             input3: input3,
+                                             input4: input4)
+        XCTAssertNotNil(result)
+    }
+    
+    func testHashPoseidon_doesNotError() {
+        let claimsTree = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+        let revocationTree = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+        let rootsTreeRoot = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+
+        let result = BabyJubjub.hashPoseidon(claimsTree: claimsTree,
+                                             revocationTree: revocationTree,
+                                             rootsTreeRoot: rootsTreeRoot)
+        XCTAssertNotNil(result)
+    }
+    
+    func testSignPoseidon_signsCorrectly() throws {
+        let privateKey = "e38c0dbe6bb723f9532b9ab847f54d8ef200ca14b3619342ff148ee9c682d580"
+        
+        let message = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+        
+        let result = BabyJubjub.signPoseidon(privateKeyHex: privateKey,
+                                             message: message)
+        
+        let expectedSignedMessage = "2a580ed879dd4d9d85868301fd6620c530ec147f386895eddec08b52fe2f3e8364f05df303496ffb4f2b64db174613f5828fde0333d18ee49965aa7cdcdd7401"
+        XCTAssertEqual(result, expectedSignedMessage)
+    }
+    
+    func testVerifyPoseidon_doesNotError() throws {
+        let privateKey = "e38c0dbe6bb723f9532b9ab847f54d8ef200ca14b3619342ff148ee9c682d580"
+        
+        let message = "17777552123799933955779906779655732241715742912184938656739573121738514868268"
+        
+        let signedMessage = "2a580ed879dd4d9d85868301fd6620c530ec147f386895eddec08b52fe2f3e8364f05df303496ffb4f2b64db174613f5828fde0333d18ee49965aa7cdcdd7401"
+        
+        let compressedSignature = try XCTUnwrap(BabyJubjub.packSignature(signature: signedMessage))
+
+        let result = BabyJubjub.verifyPoseidon(privateKey: privateKey,
+                                               compressedSignature: signedMessage,
+                                               message: message)
+        
+        XCTAssertNotNil(result)
+    }
+}
+
+extension String {
+    func padLeft(length: Int, char: String) -> String {
+        guard self.count < length else {
+            return self
+        }
+        
+        let padLength = length - self.count
+        let padding = String(repeating: char, count: padLength)
+        return padding + self
+    }
 }
